d9851dde3ddb72bdb1f233c7b5dd6146f550bfe5
Charles Rivers <UltimateSky@users.noreply.github.com>
2017-06-20 19:54:47 -0500

    Updated Vendor Weapons page to match armor page with some CSS fixes t… (#26)
    * Updated Vendor Weapons page to match armor page with some CSS fixes to address bugs on the weapons page
    
    * fixed sidebar link to reference vendor armor
    
    * Update vendorweapons.php
    
    * Changed link from /vendorarmor.php whcih doesn't exist to /vendors.php.

--- vendorweapons.php
+++ vendorweapons.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 
 require_once ('config.php');
@@ -8,35 +9,75 @@ $query = "SELECT * FROM VendorWeapon";
 $result = pg_query($query);
 
 ?>
-<html>
- <head>
-  <title>Welcome to /r/DTG/</title>
-  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/jquery.dataTables.css">  
-  
-  <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-  <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.js"></script>
-  <style>    
-         * {
-                font-size: 13px;
-                line-height: 1.4;
-        }
-        
-  </style>
- </head>
- <body>
-     <div class="container">
-     <h1>Vendor Weapons</h1>
-     <?php
+    <html>
+
+    <head>
+        <title>Welcome to /r/DTG/</title>
+        <!-- CSS -->
+        <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="./static/css/main.css">
+        <link rel="stylesheet" type="text/css" href="./static/css/nav.css?ver=3">
+        <link rel="stylesheet" type="text/css" href="./static/css/tables.css?ver=5">
+        <link rel="stylesheet" type="text/css" href="./static/css/jquery.dataTables.css">
+        <!-- JS -->
+        <script src="./static/js/jquery-3.2.1.min.js"></script>
+        <script src="./static/js/bootstrap.min.js"></script>
+        <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.js"></script>
+    </head>
+
+    <body>
+        <div class="masthead-weapons"> </div>
+        <div id="nav-wrap">
+            <div id="main-nav" class="navbar">
+                <div class="navbar-inner">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <div class="navbar-brand navbar-brand-centered"></div>
+                        </div>
+                    </div>
+                </div>
+                <!-- navbar -->
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-2 sidebar1">
+                    <div class="container sidebar1" id="sidebar">
+                        <div class="logo"> <img src="./static/img/Raid_Emblem.png" class="rounded" alt="Logo" style="max-width:175px; border:0; padding-left: 40px;"> </div>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <div class="left-navigation">
+                            <ul class="list">
+                                <h5><strong>LINKS</strong></h5>
+                                <li><a href="https://goo.gl/xgpzbX">Discuss this on Reddit</a></li>
+                                <li><a href="vendors.php">Vendor Armor</a></li>
+                            </ul>
+                            <br>
+                            <ul class="list">
+                                <h5><strong>/r/DTG LINKS</strong></h5>
+                                <li><a href="https://www.reddit.com/r/DestinyTheGame">Back to the subreddit</a></li>
+                                <li><a href="https://discord.gg/DestinyReddit">Destiny Discord</a></li>
+                                <li><a href="https://www.reddit.com/message/compose?to=%2Fr%2FDestinyTheGame&subject=DestinyReddit%20Site">Contact Info</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-10 main-content">
+                    <div class="container-fluid" id="wrapper">
+                        <h1 class="title">Vendor Weapons - 6/6/2017</h1>
+                        <br>
+                        <br>
+                        <?php
 
         echo "<div class='container'>";
         echo "<div class='row'>";
         echo "<div class='col-md-12'>";
-        
-        echo "<div class='table-responsive'>";        
-        echo "<table class='table table-bordered table-striped' id='vendorweapon'>";
+        echo "<div class='table-responsive vendor-table'>";        
+        echo "<table class='table table-bordered table-striped' id='vendorarmor'>";
         echo "<thead>";
-        echo "<tr><th>Vendor</th> <th>Name</th> <th>Type</th> <th>Perks 1</th> <th>Perks 2</th> <th>Perks 3</th> <th>Perks 4</th> <th>Perks 5</th> </tr>";
+        echo "<tr><th>Vendor</th> <th>Name</th> <th>Type</th> <th>Perks 1</th> <th>Perks 2</th> <th>Perks 3</th> <th>Perk 4</th> <th>Perk 5</th> </tr>";
         echo "</thead>";
 
         echo "<tbody>";
@@ -56,19 +97,40 @@ $result = pg_query($query);
         echo "</tbody>";
         echo "</table>";
         echo "</div>";
-
         echo "</div>";
         echo "</div>";
         echo "</div>";
-      ?>
-
-      </div>
-      <script type="text/javascript">
-        $(document).ready( function () {
-            $('#vendorweapon').DataTable({
-              "paging": false
+      ?> </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        <script type="text/javascript">
+            $(document).ready(function () {
+                $('#vendorarmor').DataTable({
+                    "paging": false
+                });
+            })
+        </script>
+        <script type="text/javascript">
+            /* navbar affix scrollspy */
+            $('#nav-wrap').height($("#nav").height());
+            $('#main-nav').affix({
+                offset: $('#main-nav').position()
             });
-        } )
-      </script>
- </body>
- </html>
+            /* Sidebar affix scrollspy */
+            /* activate sidebar */
+            $('#sidebar').affix({
+                offset: $('#main-nav').position()
+            });
+            /* activate scrollspy menu */
+            var $body = $(document.body);
+            var navHeight = $('.navbar').outerHeight(true);
+            $body.scrollspy({
+                target: '#leftCol'
+                , offset: '100px'
+            });
+        </script>
+    </body>
+
+    </html>
